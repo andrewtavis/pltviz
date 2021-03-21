c616cd48477df5e9014529274c3aef4b141dbd1e
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-03-21 12:56:17 +0100

    Update package for new structure and clean wheel

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup_args = dict(
     name="pltviz",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    version="0.1.2.2",
+    version="0.1.2.3",
     author="Andrew Tavis McAllister",
     author_email="andrew.t.mcallister@gmail.com",
     classifiers=[
